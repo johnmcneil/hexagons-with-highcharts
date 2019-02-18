afb46751d58d69d3c52a7f68f19c9b419e12a7af
jmcneil <admin@chessgraphs.com>
2019-02-18 12:44:08 -0600

    Hexegons with highcharts and highmaps.js
    Starting point for using highcharts and highmaps.js to map hexagonal space

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # hexegons with highcharts
+# using highcharts.js and highmaps.js for hexagon tiles project
+# this jsfiddle is a starting point: https://jsfiddle.net/gh/get/library/pure/highcharts/highcharts/tree/master/samples/maps/series/tilemap-gridoffset
+# an example of a hexagon map with highcharts: https://www.highcharts.com/blog/snippets/hexagon-map-usa/
+# documentation of tilemap series with highcharts: https://www.highcharts.com/docs/chart-and-series-types/tilemap-series
---
+++ hex.js
@@ -0,0 +1,55 @@
+Highcharts.chart('container', {
+	chart: {
+		type: 'tilemap',
+		height:'100%'
+	},
+
+	title: {
+		text: 'hexagons!'
+	},
+
+	subtitle {
+		text: 'the shapes that brought you honey, food of the gods'
+	},
+
+	tooltip: {
+		pointFormat: 'x: {point.x}<br />y: {point: y}'
+	},
+
+	plotOptions: {
+		series: {
+			dataLabels: {
+				enabled: true,
+				color: '#000000',
+				format: '{point.x},{point.y}'
+			},
+			borderWidth: 1,
+			borderColor: '#777'
+		}
+	},
+
+	series: [{
+		keys: ['x', 'y'],
+		color: 'teal',
+		data: [
+			[0,0],
+			[1,0],
+			[2,0],
+			[3,0],
+			[4,0],
+			[1,1],
+			[2,1],
+			[3,1],
+			[4,1],
+			[0,2],
+			[1,2],
+			[2,2],
+			[3,2],
+			[4,2],
+			[1,3],
+			[2,3],
+			[3,3],
+			[4,3]
+		]
+	}]
+}
